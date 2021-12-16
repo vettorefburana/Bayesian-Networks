e58c23121374a6993f8023714e02e3b7df72edb2
vettorefburana <74903161+vettorefburana@users.noreply.github.com>
2021-12-16 18:35:50 +0100

    Add files via upload

---
+++ requirements.r
@@ -0,0 +1,9 @@
+
+install.packages("BiocManager")
+install.packages("gRbase")
+install.packages("Hmisc")
+install.packages("ggm")
+install.packages("igraph")
+install.packages("kableExtra")
+install.packages("bnlearn")
+
